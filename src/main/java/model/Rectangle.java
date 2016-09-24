e48eb3ff018e49b8c191f784fb6616628e3d4fcf
Your Name <briksmith@earthlink.net>
2016-09-23 23:13:41 -0500

    Deffensive copying
    Now, if the point you pass into rectangle changes, it won't change
    rectangle's points.

--- src/main/java/model/Rectangle.java
+++ src/main/java/model/Rectangle.java
@@ -6,10 +6,18 @@ public class Rectangle
 	private Point upperRightPoint;
 	
 	public Rectangle(Point inLowerLeftPoint, Point inUpperRightPoint){
-		this.lowerLeftPoint = inLowerLeftPoint;
-		this.upperRightPoint = inUpperRightPoint;
+		
+		this.lowerLeftPoint = DefensiveCopyOfPoint(inLowerLeftPoint);
+		this.upperRightPoint = DefensiveCopyOfPoint(inUpperRightPoint);
 	}
 	
+	private Point DefensiveCopyOfPoint(Point inPoint)
+	{
+		int xValue = inPoint.getX();
+		int yValue = inPoint.getY();
+		return new Point(xValue, yValue);
+	}
+
 	public boolean Contains(Point inPoint){
 		
 		return false;
