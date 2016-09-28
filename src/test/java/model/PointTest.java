a431a4dbbf1353639814cafabd02145733c827f4
Your Name <briksmith@earthlink.net>
2016-09-28 10:57:18 -0500

    Added test coverage.  Committing the file Utils.
    The compareTo method was needed for debugging, but I will keep it.

--- src/test/java/model/PointTest.java
+++ src/test/java/model/PointTest.java
@@ -71,4 +71,52 @@ public class PointTest
 		Point point = new Point(1,1);
 		assertTrue("Expected out put of: X: 1, Y: 1.  Was: " + point.toString(),  point.toString().equals("X: 1, Y: 1"));
 	}
+	
+	@Test 
+	public void testEqualsEqualPoint() {
+		Point point1 = new Point(2,2);
+		Point point2 = new Point(2,2);
+		
+		assertTrue("Compare to should return zero  Did not. " + point1.toString() + " " + point2.toString(),  point1.compareTo(point2) == 0);
+	}
+	
+	@Test 
+	public void testPoint1GreaterDueToGreaterX() {
+		Point point1 = new Point(3,2);
+		Point point2 = new Point(2,2);
+		
+		assertTrue("Compare to should return 1 as point 1 has the greater X value. " + point1.toString() + " " + point2.toString(),  point1.compareTo(point2) == 1);
+	}
+	
+	@Test 
+	public void testPoint1GreaterDueToGreaterY() {
+		Point point1 = new Point(2,3);
+		Point point2 = new Point(2,2);
+		
+		assertTrue("Compare to should return 1 as point 1 has the greater X value. " + point1.toString() + " " + point2.toString(),  point1.compareTo(point2) == 1);
+	}
+	
+	@Test 
+	public void testPoint1LesserDueToLesserX() {
+		Point point1 = new Point(1,2);
+		Point point2 = new Point(2,2);
+		
+		assertTrue("Compare to should return -1  Did not. " + point1.toString() + " " + point2.toString(),  point1.compareTo(point2) == -1);
+	}
+	
+	@Test 
+	public void testPoint1LesserDueToLesserY() {
+		Point point1 = new Point(2,1);
+		Point point2 = new Point(2,2);
+		
+		assertTrue("Compare to should return -1 Did not. " + point1.toString() + " " + point2.toString(),  point1.compareTo(point2) == -1);
+	}
+	
+	@Test
+	public void testPoint1Returns0ForNonPointObject() {
+		Point point1 = new Point(2,2);
+		foo f = new foo();
+		
+		assertTrue("Should get zero for non point object. ", point1.compareTo(f) == 0);
+	}
 }
